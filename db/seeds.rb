878dcf5f4031efa2b1770504d7ebf931397b6b70
PierGiovanni Fontana <piergiovanni@cegetel.net>
2017-02-15 17:52:25 +0100

    resto quite good

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants_attributes = [
+{
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "french",
+    phone:        "0165849931"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "bèlge",
+    phone:        "0436598611"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "spanish",
+    phone:        "0158462344"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
